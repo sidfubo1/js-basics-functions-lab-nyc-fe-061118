e5d19ffcb8b91223b1d4d5c635a44049fe93b32a
sidfubo1 <sidfubo1@gmail.com>
2018-07-17 19:52:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solution in this file!
 let a=42;
 let b=264;
+let p1=0.02;
+let flat_p2=0;
 
 //Returns the number of blocks from Scuber's headquarters to the pickup location (42nd Street)
 function distanceFromHqInBlocks(x) {
@@ -20,7 +22,6 @@ function distanceTravelledInFeet(x,y){
 //Given the same starting and ending block as the previous test (hint hint), return the fare for the customer. The first four hundred feet are free. For a distance between 400 and 2000 feet, the price is 2 cents per foot (not including 400, which are free!). Then Scuber charges a flat fare for a distance over 2000 feet and under 2500 feet. Finally, Scuber does not allow any rides over 2500 feet - the function returns 'cannot travel that far' if a ride over 2500 feet is requested.
 function calculatesFarePrice(x,y){
   if(400<distanceTravelledInFeet(x,y)<2000){
-    
-    
+    return 
   }
 }
