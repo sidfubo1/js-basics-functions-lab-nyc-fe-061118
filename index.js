36d85657e10b4c3b829ad94f26b62bac811ad26f
sidfubo1 <sidfubo1@gmail.com>
2018-07-17 19:44:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function distanceFromHqInBlocks(x) {
 
 //Returns the number of feet from Scuber's headquarters to the pickup location.
 function distanceFromHqInFeet(y) {
-  distanceFromHqInBlocks(x);
+  return 264*(distanceFromHqInBlocks(x)-1);
 }
