7b2fc9fa0803e3c7d4a3257047bfc8e5bd694331
sidfubo1 <sidfubo1@gmail.com>
2018-07-17 19:40:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solution in this file!
 let a=42;
-// 
+
+//  Returns the number of blocks from Scuber's headquarters to the pickup location (42nd Street)
 function distanceFromHqInBlocks(x) {
   return Math.abs(x-a);
 }
+
