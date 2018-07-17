7496be0a87f0d06a143944a4890b0d6eb798a056
sidfubo1 <sidfubo1@gmail.com>
2018-07-17 19:46:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution in this file!
 let a=42;
+let b=264;
 
 //Returns the number of blocks from Scuber's headquarters to the pickup location (42nd Street)
 function distanceFromHqInBlocks(x) {
@@ -8,5 +9,5 @@ function distanceFromHqInBlocks(x) {
 
 //Returns the number of feet from Scuber's headquarters to the pickup location.
 function distanceFromHqInFeet(y) {
-  return distanceFromHqInBlocks(y);
+  return b*distanceFromHqInBlocks(y);
 }
