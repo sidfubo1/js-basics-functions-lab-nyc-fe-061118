5d85ac9edc4542a3925e0a0a3a64f0eac60a965a
sidfubo1 <sidfubo1@gmail.com>
2018-07-17 19:50:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,4 +17,10 @@ function distanceTravelledInFeet(x,y){
   return b*(Math.abs(y-x));
 }
 
-//
+//Given the same starting and ending block as the previous test (hint hint), return the fare for the customer. The first four hundred feet are free. For a distance between 400 and 2000 feet, the price is 2 cents per foot (not including 400, which are free!). Then Scuber charges a flat fare for a distance over 2000 feet and under 2500 feet. Finally, Scuber does not allow any rides over 2500 feet - the function returns 'cannot travel that far' if a ride over 2500 feet is requested.
+function calculatesFarePrice(x,y){
+  if(400<distanceTravelledInFeet(x,y)<2000){
+    
+    
+  }
+}
